5d55839ea884d7820a3303c2ed93b67e4430e354
amukund1 <adithyamukund@Msn.com>
2017-06-22 17:39:01 -0700

    Entry Function Working
    all details of basic entry function are working

--- myTaskListLogic.js
+++ myTaskListLogic.js
@@ -1,13 +1,20 @@
 function addToList() {
-	var div = document.createElement('div');
-	$(div).css({
-		"width": "80%",
-		"height": "30px",
-		"margin": "10px 10% 10px 10%",
-		"background-color": "green"
-	});
+	if ($("#input").val() != "") {
+		var div = document.createElement('div');
+		$(div).html($("#input").val());
+		$(div).css({
+			"width": "80%",
+			"height": "30px",
+			"margin": "10px 10% 10px 10%",
+			"background-color": "green",
+			"color": "white",
+			"font-size": "150%",
+			"font-family": "sans-serif",
+			"text-align": "center"
+		});
 	
-	$("#taskList").append(div);
+		$("#taskList").append(div);
+	}
 }
 
 $("#add").click(addToList);
