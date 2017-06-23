81cc29d10bc778d49ab6564fee81a3978450f5cb
amukund1 <adithyamukund@Msn.com>
2017-06-22 17:53:05 -0700

    Input constraints
    max length of an input defined

--- myTaskListLogic.js
+++ myTaskListLogic.js
@@ -3,18 +3,19 @@ function addToList() {
 		var div = document.createElement('div');
 		$(div).html($("#input").val());
 		$(div).css({
-			"width": "80%",
+			"width": "90%",
 			"height": "30px",
-			"margin": "10px 10% 10px 10%",
+			"margin": "10px 5% 10px 5%",
 			"background-color": "green",
 			"color": "white",
 			"font-size": "150%",
 			"font-family": "sans-serif",
-			"text-align": "center"
+			"text-align": "center",
 		});
 	
 		$("#taskList").append(div);
 	}
 }
 
+$("#input").attr("maxlength", "35");
 $("#add").click(addToList);
