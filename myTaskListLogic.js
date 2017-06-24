9f907c4ba318271f7f240394323be6338d8142d2
amukund1 <adithyamukund@Msn.com>
2017-06-24 16:19:07 -0700

    Delete functionality working
    elements can now be removed from the list

--- myTaskListLogic.js
+++ myTaskListLogic.js
@@ -1,21 +1,39 @@
 function addToList() {
 	if ($("#input").val() != "") {
-		var div = document.createElement('div');
-		$(div).html($("#input").val());
-		$(div).css({
-			"width": "90%",
+		var task = document.createElement('div');
+		//$(task).attr("class", "myTask");
+		$(task).html($("#input").val());
+		$(task).css({
+			"width": "95%",
 			"height": "30px",
-			"margin": "10px 5% 10px 5%",
+			"margin": "10px 0% 10px 2%",
 			"background-color": "green",
 			"color": "white",
 			"font-size": "150%",
 			"font-family": "sans-serif",
-			"text-align": "center",
+			"text-align": "center"
 		});
-	
-		$("#taskList").append(div);
+		
+		var del = document.createElement('button');
+		//$(del).attr("class", "myDel");
+		$(del).html("Del");
+		$(del).css({
+			"width": "10%",
+			"height": "30px",
+			"float": "right"
+		});
+		
+		$(task).append(del);
+		$("#taskList").append(task);
+		
+		$(del).click(function() {
+			del.remove();
+			task.remove();
+		});
 	}
 }
 
+
 $("#input").attr("maxlength", "35");
+
 $("#add").click(addToList);
